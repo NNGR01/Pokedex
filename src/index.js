978168a2192bd3941461a43e69baa93f82835758
NNGR01 <guzman.r.nicolas@gmail.com>
2022-01-19 12:18:54 +0000

    semifinal

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './Home';
 import Pokemons from './Pokemons';
-import Cards from './cards';
 import './App.css';
 
 
@@ -12,7 +11,6 @@ ReactDOM.render(
 <Routes>
   <Route path="/" element={<Home />} />
   <Route path="/pokemon/:id" element={<Pokemons />} />
-  <Route path="cards" element={<Cards />} />
   <Route path="*" element={<div>404 - not found</div>} />
 </Routes>
 </BrowserRouter>,
