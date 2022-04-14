cce980263845cdc4cb8ea8a7aa3c554983c06a53
Nicolas Guzman Rivera <guzman.r.nicolas@gmail.com>
2022-04-13 23:56:31 -0400

    pokecard

--- src/Pokemons.js
+++ src/Pokemons.js
@@ -1,37 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
-import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
+const Pokemons = (props) => {
 
-const Pokemons = props => {
+  const [stats, setStats] = useState([]);  
+  let params = useParams();
+  const { id } = useParams();
+  console.log(stats.abilities);
 
-    const { id } = useParams();
+  useEffect(() => {
+    getPokes()
+}, [])
 
-    const [stats, setStats] = useState([])
-
-    useEffect(() => {
-        getPoke()
-    }, [])
-
-
-      const getPoke = id => {
-        fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-          .then((response) => response.json())
-          .then((stats) => setStats(stats.results));
-      };
-
-      console.log(stats);
-    
-
-    return (
-        <div className="col-6 container my-3 text-light text-center">
-            <div className="card bg-dark py-3">
-            <h3 className="card-title">s: {stats.name}</h3>
-            <p>Gender: {stats.id}</p>
-                
-                <Link to="/"><button>Volver</button></Link>
-            </div>
-        </div>
-    )
+const getPokes = async () => {
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    const pokes = await data.json()
+    setStats(pokes)
 }
+  
+
+
+  return (
+    <div className="col-6 container my-3 text-light text-center">
+      <div className="card bg-dark py-3">
+        <h3 className="card-title">Name: {stats.name}</h3>
+        <p>order  : {stats.order}</p>
+        <p>height  : {stats.height}</p>
+        <p>weight : {stats.weight}</p>
+        <p>base_experience: {stats.base_experience}</p>
+        <img
+                  src={`https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/${stats.name}.png`}
+                  alt="pokemon"
+                />
+
+        <Link to="/">
+          <button>Volver</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Pokemons;
